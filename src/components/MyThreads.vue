9c160ca8490cf47ab572bf63b15996588cdfa8c5
seungupnoh <soungho70_@naver.com>
2020-06-26 14:41:02 +0900

    up frontend 업데이트 (백업용)

--- src/components/MyThreads.vue
+++ src/components/MyThreads.vue
@@ -8,7 +8,7 @@
         <v-container>
             <v-row dense>
                 <v-col
-                        v-for="(item, i) in $store.state.items"
+                        v-for="(item, i) in $store.state.items[0]._embedded.threads"
                         :key="i"
                         cols="12"
                 >
